38de59c83f950def19df92c3278b1d36816a4ca9
Your Name <myguell.juan@gmail.com>
2024-08-04 15:48:38 -0300

    29 commit

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menumobile = document.getElementById('menumobile');
 
-    function toggleMenu() {
+    function toggleMenu(event) {
+        if(event.type === 'touchstart') event.preventDefault();
         const nav = document.getElementById('nav');
         nav.classList.toggle('active');
+        const active = nav.classList.contains('active');
+        event.currentTarget.setAttribute('aria-expended', 'true');
+        if(active){
+
+            event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+
+        } else{
+
+            event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+
+        }
     }
 
     menumobile.addEventListener('click', toggleMenu);
+    menumobile.addEventListener('touchstart', toggleMenu);
 });
-
-document.addEventListener("zoom", function(){
-    var zomm = document.getElementsByClassName('')
-}
-)
