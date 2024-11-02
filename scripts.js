5a342ae2b2724a437f28f364ab40365c7583fc24
Your Name <myguell.juan@gmail.com>
2024-11-02 16:21:43 -0300

    46 commit

--- scripts.js
+++ scripts.js
@@ -21,3 +21,61 @@ document.addEventListener("DOMContentLoaded", function() {
     menumobile.addEventListener('click', toggleMenu);
     menumobile.addEventListener('touchstart', toggleMenu);
 });
+
+var nextBtn = document.querySelector('.prox'),
+    prevBtn = document.querySelector('.ante'),
+    carrossel = document.querySelector('.carrossel'),
+    list = document.querySelector('.list'),
+    item = document.querySelector('.item'),
+    runnignTime = document.querySelector('.timerunnig')
+
+let timeRunning = 3000
+let timeAutoNext = 7000
+
+nextBtn.onclick = function(){
+    showSlider('next')
+}
+
+prevBtn.onclick = function(){
+    showSlider('prev')
+}
+
+let runtimeOut
+
+let runNextAuto = setTimeout(() => {
+    nextBtn.click()
+}, timeAutoNext)
+
+function resetTimeAnimation() {
+    runningTime.style.animation = 'none'
+    runningTime.offsetHeight /* trigger reflow */
+    runningTime.style.animation = null 
+    runningTime.style.animation = 'runningTime 7s linear 1 forwards'
+}
+
+function showSlider(type) {
+    let sliderItemsDom = list.querySelectorAll('.carrossel .list .item')
+    if(type == 'next'){
+        list.appendChild( sliderItemsDom[0])
+        carrossel.classList.add('next')
+    }else{
+        list.prepend( sliderItemsDom[ sliderItemsDom.length - 1])
+        carrossel.classList.add('prev')
+    }
+
+    clearTimeout(runtimeOut)
+
+    runtimeOut = setTimeout(() => {
+        carrossel.classList.remove('.prox')
+        carrossel.classList.remove('.ante')
+    }, timeRunning)
+
+    clearTimeout(runNextAuto)
+    runNextAuto = setTimeout(() => {
+        nextBtn.click()
+    }, timeAutoNext)
+
+    resetTimeAnimation()
+}
+
+resetTimeAnimation()
