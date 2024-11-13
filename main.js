3904aa763d41cec398e7645c80db06b015ca3ca3
Your Name <myguell.juan@gmail.com>
2024-11-13 20:08:27 -0300

    54 commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const text = document.querySelector('.textc p');
 text.innerHTML = text.innerHTML.split("").map(
 	(char, i) =>
-		`<span style="transform:rotate(${i * 7.3}deg)">${char}</span>`
+		`<span style="transform:rotate(${i * 5.4}deg)">${char}</span>`
 ).join("")
 
 window.addEventListener("scroll", function(){
